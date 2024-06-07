41d72e6368aa3c2579a97a28b45874a47acd8256
Jeffery Omorodion <omorodionjeffery0@gmail.com>
2024-06-07 23:56:21 +0100

    setting up javascript

---
+++ index.js
@@ -0,0 +1,9 @@
+const cityName = document.getElementById('cityInput');
+const apiKey = 'ad96279adc3636cb8c2a6383890f6c5c'
+
+async function getWeatherData(city, apiKey){
+
+}
+function displayWeatherinfo(){
+    
+}
