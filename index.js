aa1239b1213dd9a399d1bd481165402f515a02fa
Jeffery Omorodion <omorodionjeffery0@gmail.com>
2024-06-08 00:35:09 +0100

    editting javascript

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const cityName = document.getElementById('cityInput');
 const apiKey = 'ad96279adc3636cb8c2a6383890f6c5c'
 
 async function getWeatherData(city, apiKey){
+    if(cityName){
+        try{
 
+        }
+        catch(error){
+            
+        };
+    }
+    else{
+        displayError('Please enter a city name')
+    }
 }
 function displayWeatherinfo(){
-    
+
+}
+function displayError(message){
+ 
 }
